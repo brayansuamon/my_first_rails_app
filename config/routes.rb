812232fa08a8a0bfb420c4e09a5c114eb75f0f28
Brayan <brayansuamon@gmail.com>
2024-11-07 20:22:00 -0500

    Routes initial test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :cars
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
+  resources :posts, only: [ :index, :show ]
+  resources :users, except: [ :index ]
+
+   # Custom routes to an specific controller action
+   get "/somepath", to: "somecontroller#someaction"
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
